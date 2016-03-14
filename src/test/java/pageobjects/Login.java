d8fc8b56d4909e9fc0559a04bfd24a7b8bab28d4
MichaelZ <cadweller2005@gmail.com>
2016-03-14 00:29:38 -0700

    First Commit: Copying from Bitbucket

---
+++ src/test/java/pageobjects/Login.java
@@ -0,0 +1,75 @@
+package pageobjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+
+
+
+public class Login {
+
+    WebDriver driver;
+
+    By usernameLocator = By.id("Email");
+    By nextButtonLoginFlow = By.id("next");
+    By passwordLocator = By.id("Passwd");
+    By signInButton = By.id("signIn");
+    By verifyLoginPageLocator = By.id(":2m");
+    By clickOnAccountButton = By.cssSelector(".gb_0a.gbii");
+    By clickOnSignOut = By.id("gb_71");
+    By verifyLoggedOutLocator = By.cssSelector(".logo-strip");
+    By clickOnCompose = By.cssSelector("div[role='button'][gh='cm']");
+    By enterEmailAddress = By.className("vO");
+    By enterSubject = By.name("subjectbox");
+    By enterBody = By.cssSelector("div[aria-label='Message Body']");
+    By clickOnSend = By.cssSelector("div[aria-label*='Send']");
+    By clickOnInbox = By.xpath("//a [@href='https://mail.google.com/mail/#inbox']");
+    //By clickOnInbox = By.partialLinkText("Inbox (1)");
+    By clickOnNewlyReceivedEmail = By.cssSelector(".yX.xY");
+    By deleteEmail = By.xpath("//*[@id=':5']/div[2]/div[1]/div/div[2]/div[3]");
+
+
+    public Login (WebDriver driver) {
+        this.driver = driver;
+        this.driver.get("https://gmail.com");
+    }
+
+    public void with (String username, String password) {
+        driver.findElement(usernameLocator).sendKeys(username);
+        driver.findElement(nextButtonLoginFlow).click();
+        driver.findElement(passwordLocator).sendKeys(password);
+        driver.findElement(signInButton).click();
+            }
+
+    public void logout () {
+        driver.findElement(clickOnAccountButton).click();
+        driver.findElement(clickOnSignOut).click();
+    }
+
+    public void composeEmail (String email, String subject, String body) {
+        driver.findElement(clickOnCompose).click();
+        driver.findElement(enterEmailAddress).sendKeys(email);
+        driver.findElement(enterSubject).sendKeys(subject);
+        driver.findElement(enterBody).sendKeys(body);
+        driver.findElement(clickOnSend).click();
+    }
+
+    public void openEmail () {
+        driver.findElement(clickOnInbox).click();
+        driver.findElement(clickOnNewlyReceivedEmail).click();
+            }
+
+    public void deleteEmail () {
+        driver.findElement(deleteEmail).click();
+            }
+
+    public Boolean verifyLoginPage () {
+        return driver.findElement(verifyLoginPageLocator).isDisplayed();
+    }
+
+    public Boolean verifyLoggedOut () {
+        return driver.findElement(verifyLoggedOutLocator).isDisplayed();
+    }
+
+
+}
---
+++ src/test/java/tests/TestLogin.java
@@ -0,0 +1,58 @@
+package tests;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import pageobjects.Login;
+
+import static org.junit.Assert.*;
+import java.util.concurrent.TimeUnit;
+
+public class TestLogin {
+    private WebDriver driver;
+    private Login login;
+
+    @Before
+    public void SetUp() {
+        driver = new FirefoxDriver();
+        login = new Login(driver);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void succesfullLogin() {
+        login.with("seleniummz@gmail.com","seleniummz12345");
+        assertTrue("element is not found",
+                login.verifyLoginPage());
+        login.logout();
+        assertTrue("Not Logged Out",
+                login.verifyLoggedOut());
+    }
+
+    @Test
+    public void succesfullSendAndReceive() {
+        login.with("seleniummz@gmail.com","seleniummz12345");
+        assertTrue("element is not found",
+                login.verifyLoginPage());
+        login.composeEmail("seleniummz@gmail.com","succesfullSendAndReceive Test Case","Any Text");
+        login.openEmail();
+        assertEquals("No Subject", "succesfullSendAndReceive Test Case",
+                driver.findElement(By.cssSelector("h2[class='hP']")).getText());
+        assertEquals("No Body Text", "Any Text",
+                driver.findElement(By.cssSelector("div[class='adn ads'] div[dir='ltr']")).getText());
+        login.deleteEmail();
+        login.logout();
+
+
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+
+}
